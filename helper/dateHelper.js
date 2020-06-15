d7b552425d67990b93a3687db4fe2f424319191d
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2020-06-16 03:27:21 +0700

    update date helper localization

--- helper/dateHelper.js
+++ helper/dateHelper.js
@@ -1,6 +1,6 @@
 const formatLocaleDateString = (date = null) => {
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  return date ? new Date(date).toLocaleDateString('id-ID', options) : new Date().toLocaleDateString('id-ID', options);
+  return date ? new Date(date).toLocaleDateString('en-US', options) : new Date().toLocaleDateString('id-ID', options);
 }
 export {
   formatLocaleDateString
