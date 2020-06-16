3954290a438bc1c27e5dc1e31d6023463116220d
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2020-06-17 00:54:31 +0700

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,6 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'content/**/*.md',
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ],
-    options: {
-      whitelist: ['dark-mode']
-    }
-  },
   theme: {},
   variants: {},
   plugins: []
