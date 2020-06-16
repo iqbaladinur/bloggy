e3535266b9cf87c2ebc6184125efe899110a32ad
Iqbal Adi Nurmansyah <iqbaladinur@gmail.com>
2020-06-16 23:18:12 +0700

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,16 @@ module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
     content: [
+      'content/**/*.md',
       'components/**/*.vue',
+      'layouts/**/*.vue',
       'pages/**/*.vue',
       'plugins/**/*.js',
       'nuxt.config.js'
-    ]
+    ],
+    options: {
+      whitelist: ['dark-mode']
+    }
   },
   theme: {},
   variants: {},
